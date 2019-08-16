5e85ece9db38b578ab4a2fc37e681d48805c554e
Francis Kip <gwadafrank@gmail.com>
2019-08-16 13:08:58 +0300

    feat(setUp): Connect to postgres
    - connect app to postgres
    - run migration
    - setup Admin
    
    [Finishes]

--- flightapi/settings.py
+++ flightapi/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'flightapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flight',
+        'USER':'francky',
+        'PASSWORD':'',
+        'HOST':'',
+        'PORT':''
+
     }
 }
 

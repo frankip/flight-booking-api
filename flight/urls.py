6ca50714ec742b3ac93b8db870cc68416bc2ad39
Francis Kip <gwadafrank@gmail.com>
2019-08-19 17:21:53 +0300

    feat(Ticket) : List,create,view, update and delete tickets - list all tickets ApiList - create new tickets ApiCreate - view Ticket detail with RertrieveUpdate destroy mixin - Update Ticket details with RertrieveUpdateDestroy mixin - Delete ticket with RertrieveUpdatedestroy
    [Finishes #167924413 ]

---
+++ flight/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from flight import views
+
+urlpatterns = [
+    path('', views.ApiRoot.as_view(),
+    name=views.ApiRoot.name),
+
+    path('tickets', views.FlightTicketList.as_view(),
+    name=views.FlightTicketList.name),
+    path('tickets/<int:pk>/', views.FlightTicketDetail.as_view(),
+    name=views.FlightTicketDetail.name)
+]
